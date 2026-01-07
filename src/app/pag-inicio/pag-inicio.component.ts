ce96eb4f0ad4dfe33ca301aec7c7e47d713f37b7
Opibl <pedro.basualto.7@gmail.com>
2026-01-07 00:12:11 -0300

    con buscar

--- src/app/pag-inicio/pag-inicio.component.ts
+++ src/app/pag-inicio/pag-inicio.component.ts
@@ -1,35 +1,49 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ServicoService } from '../servico.service';
-import { CommonModule } from '@angular/common';  // Importa CommonModule
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CARRITOService} from '../carrito.service';
 
 @Component({
   selector: 'app-pag-inicio',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './pag-inicio.component.html',
   styleUrl: './pag-inicio.component.scss'
 })
-export class PagInicioComponent {
-  id: string | null = null;
-  precio: number = 0;
-  descripcion: string = '';
-  nombre:string = '';
-  productos:any[] = [];
+export class PagInicioComponent implements OnInit {
 
-  constructor(private servicio: ServicoService) { }
+  productos: any[] = [];
+  terminoBusqueda: string = '';
+
+  // 🔔 Toast
+  mostrarToast = false;
+  mensajeToast = '';
+
+  constructor(
+    private servicio: ServicoService,
+    private carritoService: CARRITOService
+  ) {}
+
   ngOnInit(): void {
-   
-      
-   
     this.servicio.obtenerDatos().subscribe(
-      (datos: any[]) => {  // Asumo que 'datos' es un array de objetos
-      this.productos = datos;  
-    });
+      (datos: any[]) => {
+        this.productos = datos;
+      }
+    );
+  }
 
-    
-    
+  /* 🔍 FILTRO */
+  get productosFiltrados() {
+    return this.productos.filter(producto =>
+      producto.nombre
+        .toLowerCase()
+        .includes(this.terminoBusqueda.toLowerCase())
+    );
   }
 }
-
-
